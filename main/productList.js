6fe12755752a25f01a6d732b83dbdc4bfda193ac
SeongHo-C <leeseong010@naver.com>
2022-05-02 19:33:38 +0900

    20220502 상품목록 조회 수정(이미지 추가)

--- main/productList.js
+++ main/productList.js
@@ -8,20 +8,13 @@ fetch('http://182.218.194.156:8080/product?page=1&per_page=12')
     console.log(data.count);
 
     function remaindTime(endDate) {
-      console.log(endDate);
-      const date = endDate.split('T');
-      const day = date[0].split('-');
-      const time = date[1].split(':');
-
       var now = new Date();
-      var end = new Date(day[0], day[1], day[2], time[0], time[1], time[2]);
+      var end = new Date(endDate);
 
       var nt = now.getTime();
       var et = end.getTime();
-      console.log(et - nt);
 
       if (nt < et) {
-        //현재시간이 오픈시간보다 이르면 오픈시간까지의 남은 시간을 구한다.
         sec = parseInt(et - nt) / 1000;
         days = parseInt(sec / 60 / 60 / 24);
         sec = sec - days * 60 * 60 * 24;
@@ -41,11 +34,10 @@ fetch('http://182.218.194.156:8080/product?page=1&per_page=12')
         }
         return days + '일 ' + hour + '시간 ' + min + '분 ' + sec + '초';
       } else {
+        return '종료';
       }
     }
-    // setInterval(remaindTime, 1000); //1초마다 검사를 해주면 실시간으로 시간을 알 수 있다.
 
-    //const product = Array.from(data);
     function htmlAdd() {
       let testHtml = '';
       const product = data.data;
@@ -54,7 +46,9 @@ fetch('http://182.218.194.156:8080/product?page=1&per_page=12')
         htmlData +=
           '<div class="col mb-5"><a href="/main/detail.html"><div class="card h-100">';
         htmlData +=
-          '<img class="card-img-top card-size" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..."/>';
+          '<img class="card-img-top card-size" src="http://182.218.194.156:8080/image/product-' +
+          product[i].id +
+          '-0.png?type=product" alt="..."/>';
         htmlData += '<div class="card-body text-center"><div class="">';
         htmlData += '<h5 class="fw-border">' + product[i].name + '</h5>';
         htmlData +=
@@ -62,21 +56,30 @@ fetch('http://182.218.194.156:8080/product?page=1&per_page=12')
           product[i].startPrice.toLocaleString() +
           '원</h5><h5>입찰 5명</h5></div></div>';
         htmlData +=
-          '<div class ="card-footer bg-transparent ms-2"><div class="me-auto">';
+          '<div class ="card-footer bg-transparent ms-1"><div class="me-auto">';
         htmlData +=
-          '<h6 class="mt-2"> 판매자 아이디: ' +
+          '<h6 class="mt-2"> 판매자아이디: ' +
           product[i].sellerId +
-          '</h6><h6> 마감시간: ' +
-          remaindTime(product[i].endDate) +
-          '</h6>';
+          '</h6><h6 id="endTime' +
+          i +
+          '"> 마감시간: 0일 00시간 00분 00초</h6>';
         htmlData += '</div></div></div></a></div>';
-        // $('#productBox').append(htmlData);
+
         testHtml += htmlData;
       }
       $('#productBox').html(testHtml);
     }
+    htmlAdd();
 
-    setInterval(htmlAdd, 1000); //1초마다 검사를 해주면 실시간으로 시간을 알 수 있다.
+    // 마감 시간
+    function timeChange() {
+      const product = data.data;
+      for (let i = 0; i < product.length; i++) {
+        document.getElementById('endTime' + i).innerHTML =
+          '남은시간: ' + remaindTime(product[i].endDate);
+      }
+    }
+    setInterval(timeChange, 1000);
   })
   .catch(function (error) {
     console.log(error);
