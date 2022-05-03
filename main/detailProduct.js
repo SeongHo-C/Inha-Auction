5dbe97b74a77d809cc8d9c397d42ab1ec78d535b
SeongHo-C <leeseong010@naver.com>
2022-05-04 02:19:49 +0900

    20220504 상품상세 api 연결(수정), 입찰 api 연결

--- main/detailProduct.js
+++ main/detailProduct.js
@@ -1,11 +1,16 @@
 const url = location.href;
-const productId = url.substring(url.length - 1, url.length);
+const productIdSplit = url.split('=');
+const productId = productIdSplit[1];
+
 fetch('http://182.218.194.156:8080/product/' + productId)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
+
+    // detail.html에서 사용
+    const product = data.data;
 
     // 사진 추가하기
     imageAdd();
@@ -19,17 +24,15 @@ fetch('http://182.218.194.156:8080/product/' + productId)
         productId +
         '-0.png?type=product" class="d-block w-100 detail-image-size" alt="..."/></div>';
 
-      htmlData += '<div class="carousel-item">';
-      htmlData +=
-        '<img src="http://182.218.194.156:8080/image/product-' +
-        productId +
-        '-1.png?type=product" class="d-block w-100 detail-image-size" alt="..."/></div>';
-
-      htmlData += '<div class="carousel-item">';
-      htmlData +=
-        '<img src="http://182.218.194.156:8080/image/product-' +
-        productId +
-        '-2.png?type=product" class="d-block w-100 detail-image-size" alt="..."/></div>';
+      for (let i = 1; i < product.imgCnt; i++) {
+        htmlData += '<div class="carousel-item">';
+        htmlData +=
+          '<img src="http://182.218.194.156:8080/image/product-' +
+          productId +
+          '-' +
+          i +
+          '.png?type=product" class="d-block w-100 detail-image-size" alt="..."/></div>';
+      }
       $('#carouselInner').html(htmlData);
     }
 
@@ -39,24 +42,31 @@ fetch('http://182.218.194.156:8080/product/' + productId)
     function productAdd() {
       let htmlData = '';
       const product = data.data;
+      const endTime = product.endDate.split('T');
       htmlData +=
-        '<thead><tr><th>시작가</th><td style="color: blue">' +
-        product.startPrice +
-        '원</td>';
-      htmlData += '<td id="time"></td></tr></thead>';
-      htmlData += '<tbody><tr><th>상품번호</th><td>' + product.id + '</td>';
+        '<colgroup><col style="width: 20%" /><col style="width: 20%" /><col style="width: 60%" /></colgroup>';
+      htmlData +=
+        '<thead><tr><th>시작가</th><td>' + product.startPrice + '원</td>';
+      htmlData += '<td id="time" style="color: blue"></td></tr></thead>';
+      htmlData += '<tbody"><tr><th>상품번호</th><td>' + product.id + '</td>';
       htmlData +=
         '<td><a href="/main/notify.html"><button type="button">신고</button></a></td></tr>';
-      htmlData += '<tr><th>마감기한</th><td> ' + product.endDate + '</td></tr>';
+      htmlData +=
+        '<tr><th>마감기한</th><td colspan="2">' +
+        endTime[0] +
+        ' ' +
+        endTime[1] +
+        '</td></tr>';
       htmlData += '<tr><th>입찰단위</th><td>' + product.bidUnit + '</td></tr>';
       htmlData +=
-        '<tr><th>즉시구매가</th><td>' + product.instantPrice + '</td></tr>';
+        '<tr><th>즉시구매가</th><td id="instantPrice_value">' +
+        product.instantPrice +
+        '</td></tr>';
       htmlData += '<tr><th>입찰 수</th><td>' + product.bidderCnt + '</td></tr>';
       htmlData +=
         '<tr><th>판매자 ID</th><td>' +
         product.sellerId +
         '</td><td><input type="button" value="판매자 거래내역"/></td></tr></tbody>';
-
       $('#detailInfo').html(htmlData);
     }
 
@@ -104,7 +114,7 @@ fetch('http://182.218.194.156:8080/product/' + productId)
       let htmlData = '';
       const product = data.data;
       htmlData +=
-        '<h2>상품설명</h2></hr><textarea style="width: 100%; height: auto" readonly>' +
+        '<h2>상품설명</h2><hr><textarea style="width: 100%; height: auto; min-height: 200px; padding: 10px;" readonly>' +
         product.content +
         '</textarea>';
 
