3499e1bf1df040c869a8f9de281bb832b2580984
SeongHo-C <leeseong010@naver.com>
2022-05-17 23:13:39 +0900

    20220517 회원 정지 api 연결

--- admin/js/memberDeleteStop.js
+++ admin/js/memberDeleteStop.js
@@ -1,5 +1,6 @@
 // 체크박스 하나만 가능하도록
-$(document).ready(function () {
+
+$(function () {
   $('input[type="checkbox"][name="checkBid"]').click(function () {
     if ($(this).prop('checked')) {
       $('input[type="checkbox"][name="checkBid"]').prop('checked', false);
@@ -23,6 +24,35 @@ $(document).ready(function () {
           .then((data) => {
             console.log(data);
             alert(data.data);
+            location.reload(true);
+          })
+          .catch(console.log);
+      });
+
+      // 정지하기
+      const memberStopBtn = document.getElementById('memberStopBtn');
+      memberStopBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        const stopMap = new Map();
+
+        const banDate = document.getElementById('banDate').value;
+        console.log(banDate);
+        console.log(checkIdx);
+        stopMap.set(checkIdx, banDate);
+        console.log(stopMap);
+        let stopmapToArray = Object.fromEntries(stopMap);
+        fetch('http://182.218.194.156:8080/admin/members/ban', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + localStorage.getItem('token'),
+          },
+          body: JSON.stringify(stopmapToArray),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+
             location.reload(true);
           })
           .catch(console.log);
--- admin/js/memberSelect.js
+++ admin/js/memberSelect.js
@@ -21,8 +21,14 @@ fetch('http://182.218.194.156:8080/admin/members?page=1&per_page=10', {
         <td>${member[i].address}</td><td>${member[i].email}</td>
         <td><img class="card-size" src="http://182.218.194.156:8080/image/member-${member[i].id}.png?type=auth" alt="이미지 없음"/></td>
         <td><select name="state" id="state-select${member[i].id}" onchange="changeState(${member[i].id})">
-        <option value="ROLE_USER">승인 회원</option><option value="ROLE_ANONYMOUS">미승인 회원</option><option value="ROLE_ADMIN">관리자</option></select></td>
-        </tr>`;
+        <option value="ROLE_USER">승인 회원</option><option value="ROLE_ANONYMOUS">미승인 회원</option><option value="ROLE_ADMIN">관리자</option></select><br>`;
+
+          if (member[i].banDate !== null) {
+            let date = new Date(member[i].banDate).toLocaleDateString();
+
+            htmlData += `<button class="btn btn-primary text-center p-0 mt-2" onclick="stopAlert('${date}')">정지중</button>`;
+          }
+          htmlData += `</td></tr>`;
         } else {
         }
       }
@@ -48,3 +54,9 @@ fetch('http://182.218.194.156:8080/admin/members?page=1&per_page=10', {
     }
   })
   .catch(console.log);
+
+// 정지 해제일 알려주기
+function stopAlert(date) {
+  console.log(date);
+  alert(`정지해제일은 ${date} 입니다.`);
+}
